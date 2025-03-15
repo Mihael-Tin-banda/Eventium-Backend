e8503d7d0e96fa4eb67677020a355beaea6cb66a
TinNanndo <tin.jovanovic003@gmail.com>
2025-03-15 09:56:58 +0100

    cors doda

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import EventRouter from './routes/events.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/events', EventRouter);
 
